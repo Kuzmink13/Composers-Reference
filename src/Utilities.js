f814673e35e7ee219e080647ecc3064e8a296025
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-10-18 10:38:59 -0500

    Updated formatting

--- src/Utilities.js
+++ src/Utilities.js
@@ -2,12 +2,13 @@
  * A class containing basic static methods and constants
  */
 class Utilities {
-  
   static notesInOctave = 12;
-  
-  static octaveMod = function(pitch) {
-    return ((pitch % this.notesInOctave) + this.notesInOctave) % this.notesInOctave;
+
+  static octaveMod(pitch) {
+    return (
+      ((pitch % this.notesInOctave) + this.notesInOctave) % this.notesInOctave
+    );
   }
 }
 
-export default Utilities
+export default Utilities;
--- src/test/Utilities.test.js
+++ src/test/Utilities.test.js
@@ -1,13 +1,11 @@
-import Utilities from "../Utilities";
+import Utilities from '../Utilities';
 
 describe('Testing OctaveMod', () => {
-
-  test("returns the input modulus 12", () => {
+  test('returns the input modulus 12', () => {
     expect(Utilities.octaveMod(3)).toBe(3);
     expect(Utilities.octaveMod(13)).toBe(1);
     expect(Utilities.octaveMod(-2)).toBe(10);
     expect(Utilities.octaveMod(26)).toBe(2);
     expect(Utilities.octaveMod(-13)).toBe(11);
-  })
-
-})
+  });
+});
