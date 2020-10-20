e76a1b8a4661beb350e206dd7f6fb5964aca05f3
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-10-19 21:10:53 -0500

    Initialized Tailwindcss

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/main.css';
+
+ReactDOM.render(
+  <React.StrictMode></React.StrictMode>,
+  document.getElementById('root')
+);
