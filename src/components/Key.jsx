3e9ba8aeb01baa5e405baefd63b99a7b57c23a40
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-10-20 17:52:56 -0500

    Implemented multi-press

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -1,47 +1,34 @@
 import React, { Component } from 'react';
 
 class Key extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      note: props.note,
-      noteNamesOn: props.noteNamesOn,
-      isWhite: props.note.length === 1,
-      pressed: false,
-      root: false,
-    };
-    this.handlePress = this.handlePress.bind(this);
-    this.handleRootPress = this.handleRootPress.bind(this);
-  }
-
   getElement() {
-    return this.state.isWhite
-      ? this.getKeyStateWhite()
-      : this.getKeyStateBlack();
+    return this.props.noteName.length === 1
+      ? this.getKeyElementWhite()
+      : this.getKeyElementBlack();
   }
 
-  getKeyStateWhite() {
+  getKeyElementWhite() {
     const base = 'h-64 w-10 border rounded-md shadow-md flex justify-center';
     const text = 'self-end p-2 font-semibold text-lg';
     switch (true) {
-      case this.state.root:
+      case this.props.index === this.props.root:
         return (
           <div
             className={`${base} bg-blue-400 border-blue-500 hover:bg-blue-300 hover:border-blue-400`}
           >
             <span className={`${text} text-blue-900`}>
-              {this.state.noteNamesOn && this.state.note}
+              {this.props.noteNamesOn && this.props.noteName}
             </span>
           </div>
         );
 
-      case this.state.pressed:
+      case this.props.pressed:
         return (
           <div
             className={`${base} bg-orange-400 border-orange-500 hover:bg-orange-300 hover:border-orange-400`}
           >
             <span className={`${text} text-orange-900`}>
-              {this.state.noteNamesOn && this.state.note}
+              {this.props.noteNamesOn && this.props.noteName}
             </span>
           </div>
         );
@@ -52,24 +39,24 @@ class Key extends Component {
             className={`${base} bg-white border-gray-400 hover:bg-purple-200 hover:border-purple-300`}
           >
             <span className={`${text} text-gray-800`}>
-              {this.state.noteNamesOn && this.state.note}
+              {this.props.noteNamesOn && this.props.noteName}
             </span>
           </div>
         );
     }
   }
 
-  getKeyStateBlack() {
+  getKeyElementBlack() {
     const base = 'h-40 w-6 -mx-3 border rounded-md shadow-md';
     switch (true) {
-      case this.state.root:
+      case this.props.index === this.props.root:
         return (
           <div
             className={`${base} bg-blue-800 border-blue-900 hover:bg-blue-700 hover:border-gray-800`}
           ></div>
         );
 
-      case this.state.pressed:
+      case this.props.pressed:
         return (
           <div
             className={`${base} bg-orange-800 border-orange-900 hover:bg-orange-700 hover:border-orange-800`}
@@ -85,26 +72,12 @@ class Key extends Component {
     }
   }
 
-  handlePress() {
-    this.setState((state) => ({
-      pressed: !state.pressed,
-      root: false,
-    }));
-  }
-
-  handleRootPress() {
-    this.setState((state) => ({
-      pressed: !state.pressed || !state.root,
-      root: !state.pressed || !state.root,
-    }));
-  }
-
   render() {
     return (
       <div
-        onClick={this.handlePress}
-        onDoubleClick={this.handleRootPress}
-        className={this.state.isWhite ? 'z-0' : 'z-10'}
+        onClick={() => this.props.handlePress(this.props.index)}
+        // onDoubleClick={this.handleRootPress}
+        className={this.props.noteName.length === 1 ? 'z-0' : 'z-10'}
       >
         {this.getElement()}
       </div>
--- src/components/Keys.jsx
+++ src/components/Keys.jsx
@@ -3,12 +3,40 @@ import Key from './Key';
 import Utilities from '../Utilities';
 
 class Keys extends Component {
-  state = {
-    noteNamesOn: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteNamesOn: false,
+      pressedArray: Array(12).fill(false),
+      root: undefined,
+    };
+    this.handlePress = this.handlePress.bind(this);
+  }
+
+  handlePress(index) {
+    this.setState((state) => ({
+      pressedArray: state.pressedArray.map((el, i) => (i === index ? !el : el)),
+    }));
+  }
+
+  // handleRootPress() {
+  //   this.setState((state) => ({
+  //     pressed: !state.pressed || !state.root,
+  //     root: !state.pressed || !state.root,
+  //   }));
+  // }
+
   render() {
-    let sequence = Utilities.noteNames.map((el) => (
-      <Key noteNamesOn={this.state.noteNamesOn} note={el} />
+    let sequence = Utilities.noteNames.map((el, i) => (
+      <Key
+        key={i}
+        noteNamesOn={this.state.noteNamesOn}
+        noteName={el}
+        index={i}
+        root={this.state.root}
+        pressed={this.state.pressedArray[i]}
+        handlePress={this.handlePress}
+      />
     ));
     return (
       <div className="p-8 flex flex-row justify-center h-auto border-b border-gray-400">
