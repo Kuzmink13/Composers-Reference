a082a727f25cf9e335be99080410c47c28ce8810
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-10-20 10:10:52 -0500

    initialized app structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/main.css';
+import Navbar from './components/Navbar';
+import Keys from './components/Keys';
 
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <div className="flex flex-col h-screen bg-gray-100">
+      <Navbar />
+      <div className="flex justify-center flex-grow">
+        <div className="h-full w-full lg:max-w-screen-lg">
+          <Keys />
+        </div>
+      </div>
+    </div>
+  </React.StrictMode>,
   document.getElementById('root')
 );
