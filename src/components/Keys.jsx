9c0c1e18bc4d449a6458b81dcbf73673ad111a55
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-10-21 10:36:34 -0500

    Moved state and methods to App.jsx

--- src/components/Keys.jsx
+++ src/components/Keys.jsx
@@ -3,44 +3,17 @@ import Key from './Key';
 import Utilities from '../Utilities';
 
 class Keys extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      noteNamesOn: false,
-      pressedArray: Array(12).fill(false),
-      root: undefined,
-    };
-    this.handlePress = this.handlePress.bind(this);
-    this.handleRootPress = this.handleRootPress.bind(this);
-  }
-
-  handlePress(index) {
-    this.setState((state) => ({
-      pressedArray: state.pressedArray.map((el, i) => (i === index ? !el : el)),
-      root: index === state.root ? undefined : state.root,
-    }));
-  }
-
-  handleRootPress(index) {
-    this.setState((state) => ({
-      pressedArray: state.pressedArray.map((el, i) =>
-        i === index ? (i === state.root ? false : true) : el
-      ),
-      root: index === state.root ? undefined : index,
-    }));
-  }
-
   render() {
     let sequence = Utilities.noteNames.map((el, i) => (
       <Key
         key={i}
-        noteNamesOn={this.state.noteNamesOn}
-        noteName={el}
         index={i}
-        root={this.state.root}
-        pressed={this.state.pressedArray[i]}
-        handlePress={this.handlePress}
-        handleRootPress={this.handleRootPress}
+        noteName={el}
+        noteNamesOn={this.props.noteNamesOn}
+        root={this.props.root}
+        pressed={this.props.keysPressed[i]}
+        handlePress={this.props.handlePress}
+        handleRootPress={this.props.handleRootPress}
       />
     ));
     return (
