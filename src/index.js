ae6dba54732f63897c0304495aeb4ff8028bf53b
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-10-21 10:35:33 -0500

    Moved html structure into App.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 import './assets/main.css';
-import Navbar from './components/Navbar';
-import Keys from './components/Keys';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="flex flex-col h-screen bg-gray-100">
-      <Navbar />
-      <div className="flex justify-center flex-grow">
-        <div className="h-full w-full lg:max-w-screen-lg">
-          <Keys />
-        </div>
-      </div>
-    </div>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
