8fef1d955689f2c019bcf40a163e582741409fb7
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-10-26 09:06:33 -0500

    Merged ModeBlock and Staff components

--- src/components/ModeBlock.jsx
+++ src/components/ModeBlock.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import Staff from './Staff';
+import VexScale from '../VexScale';
 
 class ModeBlock extends Component {
   render() {
     return (
       <div className=" h-32 m-2 p-2 bg-white border border-gray-400 rounded-lg text-center text-gray-800">
         {this.props.modeName}
-        <Staff
-          pitchCenter={this.props.pitchCenter}
-          absolutePitches={this.props.absolutePitches}
-          modeCode={this.props.modeCode}
-          modeName={this.props.modeName}
-        />
+        <div id={this.props.modeName}></div>
       </div>
+    );
+  }
+  componentDidMount() {
+    VexScale.generateStaff(
+      this.props.modeName,
+      this.props.pitchCenter,
+      this.props.modeCode,
+      this.props.absolutePitches
     );
   }
 }
