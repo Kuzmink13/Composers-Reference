1b7d43fa7ae21edb6bcd07d7f7bfe9215d294fb5
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-10-29 08:26:15 -0500

    Converted Navbar to functional component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div className="p-2 bg-white border-b border-gray-400">
-        <h1 className="text-2xl font-bold tracking-widest text-center text-gray-800">
-          COMPOSER'S REFERENCE
-        </h1>
-      </div>
-    );
-  }
-}
+const Navbar = () => (
+  <div className="p-2 bg-white border-b border-gray-400">
+    <h1 className="text-2xl font-bold tracking-widest text-center text-gray-800">
+      COMPOSER'S REFERENCE
+    </h1>
+  </div>
+);
 
 export default Navbar;
