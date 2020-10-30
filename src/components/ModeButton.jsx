a1994fefa05f7c3e1d18cdbce1c466a60beb8053
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-10-30 08:38:36 -0500

    Converted ModeButton.jsx to functional component

--- src/components/ModeButton.jsx
+++ src/components/ModeButton.jsx
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ModeButton extends Component {
-  render() {
-    const isSelected = this.props.scaleList === this.props.selectedScaleList;
+function ModeButton(props) {
+  const isSelected = props.scaleList === props.selectedScaleList;
 
-    return (
-      <div
-        onClick={() => this.props.handleSelectorChange(this.props.scaleList)}
-        className={`p-3 mx-0 sm:mx-6 lg:mx-12 font-semibold tracking-wider ${
-          isSelected ? 'border-t border-gray-900' : 'text-gray-500'
+  return (
+    <div
+      onClick={() => props.handleSelectorChange(props.scaleList)}
+      className={`p-3 mx-0 sm:mx-6 lg:mx-12 font-semibold tracking-wider ${
+        isSelected ? 'border-t border-gray-900' : 'text-gray-500'
+      }`}
+    >
+      {props.scaleListString}-NOTE
+      <span
+        className={`text-white text-xs ml-3 py-1 px-2 align-text-middle rounded-full shadow-md ${
+          isSelected ? 'bg-gray-700' : 'bg-gray-400'
         }`}
       >
-        {this.props.scaleListString}-NOTE
-        <span
-          className={`text-white text-xs ml-3 py-1 px-2 align-text-middle rounded-full shadow-md ${
-            isSelected ? 'bg-gray-700' : 'bg-gray-400'
-          }`}
-        >
-          {this.props.listSize}
-        </span>
-      </div>
-    );
-  }
+        {props.listSize}
+      </span>
+    </div>
+  );
 }
 
 export default ModeButton;
