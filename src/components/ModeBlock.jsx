111454b85d72e8ee5f399adba003bb76e28fbedf
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-10-30 09:32:14 -0500

    Converted ModeBlock.jsx to functional component

--- src/components/ModeBlock.jsx
+++ src/components/ModeBlock.jsx
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import VexScale from '../VexScale';
 
-class ModeBlock extends Component {
-  render() {
-    return (
-      <div className=" h-32 m-2 p-2 bg-white border border-gray-400 rounded-lg text-center text-gray-800">
-        {this.props.modeName}
-        <div id={this.props.modeName}></div>
-      </div>
-    );
-  }
-  componentDidMount() {
+function ModeBlock(props) {
+  useEffect(() => {
     VexScale.generateStaff(
-      this.props.modeName,
-      this.props.pitchCenter,
-      this.props.modeCode,
-      this.props.absolutePitches
+      props.modeName,
+      props.pitchCenter,
+      props.modeCode,
+      props.absolutePitches
     );
-  }
+  });
+
+  return (
+    <div className=" h-32 m-2 p-2 bg-white border border-gray-400 rounded-lg text-center text-gray-800">
+      {props.modeName}
+      <div id={props.modeName}></div>
+    </div>
+  );
 }
 
 export default ModeBlock;
