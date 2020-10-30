0b71cb2e37c4b7f5300573166ab0db2b6839b0cf
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-10-30 08:36:19 -0500

    Converted ButtonPanel.jsx to functional component

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ModeButton from './ModeButton';
 
-class ButtonPanel extends Component {
-  render() {
-    let buttons = ['SIX', 'SEVEN', 'EIGHT'];
-    return (
-      <div className="flex flex-row justify-center">
-        {buttons.map((scaleListString, i) => (
-          <ModeButton
-            key={i}
-            scaleList={i}
-            scaleListString={scaleListString}
-            selectedScaleList={this.props.selectedScaleList}
-            listSize={this.props.filteredLists[i].length}
-            handleSelectorChange={this.props.handleSelectorChange}
-          />
-        ))}
-      </div>
-    );
-  }
+function ButtonPanel(props) {
+  const buttons = ['SIX', 'SEVEN', 'EIGHT'];
+
+  return (
+    <div className="flex flex-row justify-center">
+      {buttons.map((scaleListString, i) => (
+        <ModeButton
+          key={i}
+          scaleList={i}
+          scaleListString={scaleListString}
+          selectedScaleList={props.selectedScaleList}
+          listSize={props.filteredLists[i].length}
+          handleSelectorChange={props.handleSelectorChange}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ButtonPanel;
