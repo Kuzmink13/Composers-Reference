847c85649f39e57ab8950fc8732d73d8a2e9cb5f
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-11-13 11:14:32 -0600

    Styled ModeCard.jsx

--- src/components/ModeCard.jsx
+++ src/components/ModeCard.jsx
@@ -11,16 +11,24 @@ function ModeCard(props) {
   );
 
   return (
-    <div className="relative flex flex-col p-8 items-center w-full max-w-md m-auto border border-gray-400 bg-white rounded-lg shadow-xl">
-      {props.modeName}
+    <div className="text-gray-800 relative flex flex-col py-8 items-center w-full max-w-md m-auto border border-gray-400 bg-white rounded-lg shadow-xl">
+      <div className="text-lg font-bold uppercase tracking-widest">
+        {props.modeName}
+      </div>
       <VexStaff {...props} />
-      {Array.from(modeChords, ([chord, names, notes]) => (
-        <div key={chord} className="flex justify-between w-full">
-          <div>{`${keyPrefix}${chord}`.trim()}</div>
-          <div>{names.join('-')}</div>
-          <div>{notes.join(' ')}</div>
-        </div>
-      ))}
+      <div className="mt-3">
+        {Array.from(modeChords, ([chord, names, degrees]) => (
+          <div key={chord} className="grid grid-cols-3 auto-cols-min">
+            <div className="font-semibold text-center px-3 pt-1 border-r border-gray-400">
+              {`${keyPrefix}${chord}`.trim()}
+            </div>
+            <div className="text-center px-3 pt-1 border-r border-gray-400">
+              {names.join('-')}
+            </div>
+            <div className="text-center px-3 pt-1">{degrees.join('-')}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
