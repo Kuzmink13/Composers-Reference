7a5a395110bae040a226e83c79bba756741dad5f
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-11-23 10:48:22 -0600

    Cleaned-up html-tags in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,17 +36,21 @@ function Navbar(props) {
   });
 
   return (
-    <header className="py-2 px-auto bg-white border-b border-gray-400">
+    <header className="py-1 px-auto bg-white border-b border-gray-400">
       <div className="relative mx-auto px-8 lg:max-w-screen-lg text-gray-800">
         <div className="flex justify-between items-center">
+          {/* TITLE */}
           <h1 className="text-xl sm:text-2xl font-bold tracking-widest text-center">
             COMPOSER'S REFERENCE
           </h1>
+
+          {/* NAVIGATION BUTTONS */}
           <nav className="flex text-gray-600">
+            {/* RESET BUTTON */}
             <button
-              onClick={props.clearAll}
+              name="reset note selection"
               className="tab-selection p-2 hover:text-gray-800 sm:mr-2"
-              alt="reset note selection"
+              onClick={props.clearAll}
             >
               <svg
                 className="fill-current h-5 w-5 z-30 cursor-pointer"
@@ -55,10 +59,12 @@ function Navbar(props) {
                 <path d="M14.66 15.66A8 8 0 1 1 17 10h-2a6 6 0 1 0-1.76 4.24l1.42 1.42zM12 10h8l-4 4-4-4z" />
               </svg>
             </button>
+
+            {/* OPTIONS BUTTON */}
             <button
-              onClick={optionsHandler}
+              name="options"
               className="tab-selection p-2 hover:text-gray-800 sm:mr-2"
-              alt="options"
+              onClick={optionsHandler}
             >
               <svg
                 className="fill-current h-5 w-5 z-30 cursor-pointer"
@@ -67,10 +73,12 @@ function Navbar(props) {
                 <path d="M3.94 6.5L2.22 3.64l1.42-1.42L6.5 3.94c.52-.3 1.1-.54 1.7-.7L9 0h2l.8 3.24c.6.16 1.18.4 1.7.7l2.86-1.72 1.42 1.42-1.72 2.86c.3.52.54 1.1.7 1.7L20 9v2l-3.24.8c-.16.6-.4 1.18-.7 1.7l1.72 2.86-1.42 1.42-2.86-1.72c-.52.3-1.1.54-1.7.7L11 20H9l-.8-3.24c-.6-.16-1.18-.4-1.7-.7l-2.86 1.72-1.42-1.42 1.72-2.86c-.3-.52-.54-1.1-.7-1.7L0 11V9l3.24-.8c.16-.6.4-1.18.7-1.7zM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
               </svg>
             </button>
+
+            {/* MENU BUTTON */}
             <button
-              onClick={menuHandler}
+              name="menu"
               className="tab-selection p-2 hover:text-gray-800"
-              alt="menu"
+              onClick={menuHandler}
             >
               <svg
                 className="fill-current h-5 w-5 z-30 cursor-pointer"
@@ -81,6 +89,8 @@ function Navbar(props) {
             </button>
           </nav>
         </div>
+
+        {/* OTPTIONS/MENU DROP-DOWNS */}
         {(optionsIsOpen || menuIsOpen) && (
           <div
             className="fixed h-full w-full inset-0 z-20"
