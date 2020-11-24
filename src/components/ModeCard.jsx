2f8cb0e6216f39b7f9489e1e1037e634ef42066b
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-11-24 08:41:35 -0600

    Cleaned-up HTML tags in ModeCard.jsx

--- src/components/ModeCard.jsx
+++ src/components/ModeCard.jsx
@@ -24,35 +24,45 @@ function ModeCard(props) {
       className="text-gray-800 relative flex flex-col py-8 items-center w-full max-w-md m-auto border border-gray-400 bg-white rounded-lg shadow-xl"
     >
       {/* Mode Card Heading */}
-      <div className="text-lg font-bold uppercase tracking-widest">
-        {props.modeName}
-      </div>
-      <div className="italic tracking-wider lowercase mb-3">{`from the ${props.parentTonality} scale family`}</div>
+      <hgroup>
+        <h2 className="text-lg font-bold uppercase tracking-widest text-center">
+          {props.modeName}
+        </h2>
+        <h3 className="italic tracking-wider lowercase mb-3">
+          {`from the ${props.parentTonality} scale family`}
+        </h3>
+      </hgroup>
 
       <VexStaff {...props} />
 
       {/* Chord table */}
-      <div className="max-h-card mt-5 overflow-y-auto scrolling-auto">
-        {Array.from(modeChords, ([chord, names, degrees]) => (
-          <Fragment key={chord}>
-            {/* row-divider */}
-            {isThreeNoteChord(names) && !isFirstChord(chord) && (
-              <div className="border-t border-gray-400 mt-2 mb-1" />
-            )}
+      <table className="mt-5 block max-h-card overflow-y-auto scrolling-auto">
+        <tbody>
+          {Array.from(modeChords, ([chord, names, degrees]) => (
+            <Fragment key={chord}>
+              {/* row-divider */}
+              {isThreeNoteChord(names) && !isFirstChord(chord) && (
+                <tr>
+                  <th colSpan="3" scope="row">
+                    <hr className="border-t border-gray-400 mt-2 mb-1" />
+                  </th>
+                </tr>
+              )}
 
-            {/* table row */}
-            <div key={chord} className="grid grid-cols-3 auto-cols-min">
-              <div className="font-semibold text-center px-3 pt-1 border-r border-gray-400">
-                {`${chordRoot}${chord}`.trim()}
-              </div>
-              <div className="text-center px-3 pt-1 border-r border-gray-400">
-                {names.join('-')}
-              </div>
-              <div className="text-center px-3 pt-1">{degrees.join('-')}</div>
-            </div>
-          </Fragment>
-        ))}
-      </div>
+              {/* table row */}
+              <tr key={chord}>
+                <th className="font-semibold text-center px-3 pt-1 border-r border-gray-400">
+                  {`${chordRoot}${chord}`.trim()}
+                </th>
+                <td className="text-center px-3 pt-1 border-r border-gray-400">
+                  {names.join('-')}
+                </td>
+                <td className="text-center px-3 pt-1">{degrees.join('-')}</td>
+              </tr>
+            </Fragment>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
