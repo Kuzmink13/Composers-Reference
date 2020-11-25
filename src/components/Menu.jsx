a94a029f630c68ea4bcbaec6c57c9e1655fd573f
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-11-25 09:45:41 -0600

    Updated tab-selection and hover styles in Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
 function Menu() {
+  const links = [
+    { title: 'About' },
+    { title: 'Meet the Scales' },
+    { title: 'FAQ' },
+    { title: 'Contact' },
+  ];
+
   return (
     <ul className="drop-down w-56 mt-10 mr-10 py-2">
-      <li className="menu-item">About</li>
-      <li className="menu-item">Meet the Scales</li>
-      <li className="menu-item">FAQ</li>
-      <li className="menu-item">Contact</li>
+      {links.map((el) => (
+        <li key={el.title} className="px-4 py-2">
+          <span className="tab-selection px-2 py-1 cursor-pointer" tabIndex="0">
+            {el.title}
+          </span>
+        </li>
+      ))}
     </ul>
   );
 }
