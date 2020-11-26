2c0b75601bc2ad1e0fef38eeb11e15ca064f3a89
KuzminK13 <Konstantinkuzmin9@gmail.com>
2020-11-26 10:37:31 -0600

    Added hover:underline to Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -35,7 +35,10 @@ function Menu() {
     <ul id="menu" className="drop-down w-56 mt-10 mr-10 py-2">
       {links.map((el) => (
         <li key={el.title} className="px-4 py-2">
-          <span className="tab-selection px-2 py-1 cursor-pointer" tabIndex="0">
+          <span
+            className="tab-selection px-2 py-1 cursor-pointer hover:underline"
+            tabIndex="0"
+          >
             {el.title}
           </span>
         </li>
